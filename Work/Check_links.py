5c8c6e2e06cc5ef2a9ab40f10a7839db737f9203
Nate-River-W <67866108+Nate-River-W@users.noreply.github.com>
2021-06-09 09:38:33 +0600

    Update Check_links.py

--- Work/Check_links.py
+++ Work/Check_links.py
@@ -49,4 +49,4 @@ for url in list_links:
         dontworklist.append(f'{url} - Не работает')
 print('\n*****    Cписок не работающих ссылок    ******\n')
 print('\n'.join(dontworklist))
-print('5')
+
