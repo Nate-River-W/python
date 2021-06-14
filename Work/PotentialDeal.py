4bf681223ae6b59c8dcc0d8bf164c590727c7476
Nate-River-W <67866108+Nate-River-W@users.noreply.github.com>
2021-06-14 22:16:47 +0600

    Update PotentialDeal.py

--- Work/PotentialDeal.py
+++ Work/PotentialDeal.py
@@ -2,15 +2,15 @@ import json
 import requests
 
 data = {
-'binIin': "131140025304",
-'catalogId': 3345,
-'email': "nico.96@mail.ru",
-'isUseRecommendPo': False,
-'name': "Тест не звонить",
-'phone': "77075553518",
-'productGroupDivision': "2",
-'registrationCheckbox': False,
-'serverId': 1
+    'binIin': "131140025304",
+    'catalogId': 3345,
+    'email': "nico.96@mail.ru",
+    'isUseRecommendPo': False,
+    'name': "Тест не звонить",
+    'phone': "77075553518",
+    'productGroupDivision': "2",
+    'registrationCheckbox': False,
+    'serverId': 1
 
 }
 
