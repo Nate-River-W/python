9ad4ed4189ae00a6bcd68cc6ee1f2d9b3147e125
Nate-River-W <67866108+Nate-River-W@users.noreply.github.com>
2021-06-14 18:19:39 +0600

    Update Pd.py

--- Work/Pd.py
+++ Work/Pd.py
@@ -16,5 +16,30 @@ data = {
 
 
 r = requests.post('https://integration.ismet.kz/bpmn/api/v2/public/potentialDeal/mobile', json=data)
+r = r.json()
 
-print(r.json())
+a = input('sms code:  ')
+data2 = {
+
+     'type': 'potentialDeal',
+     'emailOrPhone': 77075553518,
+     'code': a,
+     'email': 'nico.96@mail.ru'
+
+}
+r2 = requests.post('https://integration.ismet.kz/bpmn/api/v1/public/accountRecover/verifyCode', json=data2)
+
+r2 = r2.json()
+r2 = r2['header']
+
+data3 = {
+
+  "id": r['id'],
+  "token": r2['errorText'],
+  "phone": "77075553518"
+
+
+}
+r3 = requests.post("https://integration.ismet.kz/bpmn/api/v2/public/potentialDeal/submit", json=data3)
+
+print(r3.json())
