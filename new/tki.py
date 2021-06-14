1847cad166a80a24edc3cf72d04459e1f61b38e4
Nate-River-W <67866108+Nate-River-W@users.noreply.github.com>
2021-06-14 23:38:41 +0600

    Update tki.py

--- new/tki.py
+++ new/tki.py
@@ -5,34 +5,33 @@ import json
 #
 user = fake_useragent.UserAgent().random
 headers = {
-     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
+     'user-agent': user
 }
-#
-# r = session.get('https://expw.net/ph/index.php', headers=headers)
-# # s_token = session.cookies.get('JSESSIONID')
-#
-# data1 = {
-#     'dologin': 1,
-#     'username': 'nearpulsar',
-#     'password': 'Nateriverisgod1996'
+session = requests.Session()
+
+r = session.get('https://op.ismet.kz/back/', headers=headers)
+s_token = session.cookies.get('JSESSIONID')
+
+
+url = 'https://op.ismet.kz/back/zkau;jsessionid=' + s_token
+
+
+data1 = {
+    'data_0': {"value":"ntgthm21yt33elfkzn","start":18},
+    'data_1': {"value":"131140025304","start":12}
+}
+response = session.post(url, json=data1, headers=headers)
+
+# f = open('file.html', 'w', encoding="utf-8")
+# f.write(response1.text)
+print(response.text)
+
+# data = {
+#     "query": "131140025304",
+#     "from": 0,
+#     "size": 50
 # }
 #
 #
-# response = session.post("https://expw.net/ph/index.php", data=data1, headers=headers)
-#
-# #response1 = session.post("https://op.ismet.kz/back/zkau;jsessionid=", headers=headers)
-#
-# # f = open('file.html', 'w', encoding="utf-8")
-# # f.write(response1.text)
-# print(response.text)
-
-data = {
-    "query": "131140025304",
-    "from": 0,
-    "size": 50
-}
-
-session = requests.Session()
-s = session.get('https://www.ismet.kz', headers=headers)
-r = requests.post('https://integration.ismet.kz/bpmn/api/v2/public/search/all', json=data, headers=headers)
-print(json.dumps(r.json(), indent=2, ensure_ascii=False))
+# r = requests.post('https://integration.ismet.kz/bpmn/api/v2/public/search/all', json=data, headers=headers)
+# print(json.dumps(r.json(), indent=2, ensure_ascii=False))
