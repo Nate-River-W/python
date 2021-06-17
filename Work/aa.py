f38c05d872f0c0c2c367f552d125c62429d8cd71
Nate-River-W <67866108+Nate-River-W@users.noreply.github.com>
2021-06-17 18:01:01 +0600

    Update aa.py

--- Work/aa.py
+++ Work/aa.py
@@ -1,5 +1,44 @@
-def test():
-    a = [1, 2, 3]
-    return a
+import requests
 
-print(len(test()))
+url = 'https://idp.ismet.kz/auth/realms/ocp/protocol/openid-connect/token'
+
+data_for_token = {
+    'client_id': 'aem_system',
+    'grant_type': 'password',
+    'client_secret': '0b9acb8f-8a12-4c6a-ad17-4c283215fc23',
+    'username': 'aibektokmanov@yandex.kz',
+    'password': 'Qwerty123'
+
+}
+
+data_for_callback = {
+    'name': 'Тест не звонить',
+    'phone': '77075553518',
+    'email': 'nico.96@mail.ru',
+    'server_id': 1,
+    'catalog_id': 3734,
+
+
+}
+
+
+def get_response():
+    response = requests.post(url, data=data_for_token)
+    response = response.json()['access_token']
+    token = {
+
+        'Authorization': f'Bearer {response}'
+
+        }
+    return token
+
+
+def send_callback():
+    token = get_response()
+    end_json = requests.post('https://integration.i-smet.kz/bpmn/api/v2/system/kzCallBack/request',
+                             data=data_for_callback,
+                             headers=token)
+    return end_json
+
+
+print(send_callback())
